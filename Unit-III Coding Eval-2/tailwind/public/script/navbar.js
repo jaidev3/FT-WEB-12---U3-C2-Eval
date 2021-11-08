a03af8b3ea60e33a8c787abeb320aa6435084654
jaidev3 <91534937+jaidev3@users.noreply.github.com>
2021-11-08 17:39:41 +0530

    coding eval -2 unit 3

---
+++ Unit-III Coding Eval-2/tailwind/public/script/navbar.js
@@ -0,0 +1,15 @@
+const navbar = () => {
+  return `
+
+<div>
+        <div class="flex justify-around  bg-green-300 h-14">
+        <div class="text-blue-500 self-center space-x-2.5"><span>Home</span> <spane>Menu</spane></div>
+        <h1 class="text-2xl self-center ">Food App</h1>
+        <div class="space-x-3.5 self-center"><span class="">Login</span> <span class="">Signup</span></div>
+        </div>
+</div>
+
+`
+};
+
+export default navbar;
